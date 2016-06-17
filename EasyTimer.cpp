93d2c56aadfb26ebc753ecb4c4be49e43ae8863a
Christian Schweichler <christian.schweichler@gmx.de>
2016-06-17 23:40:45 +0200

    setTimeout und setInterval implementiert;

--- EasyTimer.cpp
+++ EasyTimer.cpp
@@ -3,16 +3,43 @@
 
 EasyTimer::EasyTimer(int maxTimers) {
   this->maxTimers = maxTimers;
+  timers = new timer[maxTimers];
 }
 
 int EasyTimer::setInterval(unsigned long milliseconds, callback_function callback) {
-    return 0;
+    return addTimer(milliseconds, callback, INTERVAL);
 }
 
 int EasyTimer::setTimeout(unsigned long milliseconds, callback_function callback) {
-    return 0;
+    return addTimer(milliseconds, callback, TIMEOUT);
 }
 
 void EasyTimer::update() {
-    
+    elapsedMilliseconds = millis();
+    for(int i = 0; i < this->maxTimers; i++) {
+        if (timers[i].alive) {
+            //ToDo: add code
+        }
+    }
+}
+
+int EasyTimer::findFreeSlot() {
+    for (int i = 0; i < this->maxTimers; i++) {
+        if (timers[i].alive == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int EasyTimer::addTimer(long milliseconds, callback_function callback, TimerType type) {
+    int slot = findFreeSlot();
+    if (slot != -1) {
+        timers[slot].alive = 1;
+        timers[slot].milliseconds = milliseconds;
+        timers[slot].elapsedMilliseconds = 0;
+        timers[slot].callback = callback;
+        timers[slot].type = type;
+    }
+    return slot;
 }
--- EasyTimer.h
+++ EasyTimer.h
@@ -4,7 +4,7 @@
 class EasyTimer {
 
     typedef void (*callback_function)();
-    
+            
     public:
         EasyTimer(int maxTimers);
         int setInterval(unsigned long milliseconds, callback_function callback);
@@ -12,9 +12,24 @@ class EasyTimer {
         void update();
 
     private:
-      int maxTimers;
-      
         
+        enum TimerType {INTERVAL, TIMEOUT};
+        
+        struct timer {
+            int alive = 0;
+            long milliseconds;
+            long elapsedMilliseconds;
+            callback_function callback;
+            TimerType type;
+        };
+        
+        int maxTimers;
+        timer* timers;
+        long elapsedMilliseconds;
+        
+        int findFreeSlot();            
+        int addTimer(long milliseconds, callback_function callback, TimerType type);
+              
 };
 
 #endif
