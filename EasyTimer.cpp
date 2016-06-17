c1d6a67bcb8294c94976904c7eba60e90349590b
Christian Schweichler <christian.schweichler@gmx.de>
2016-06-17 22:30:07 +0200

    die Basis

---
+++ EasyTimer.cpp
@@ -0,0 +1,19 @@
+#include "EasyTimer.h"
+#include "Arduino.h"
+
+EasyTimer::EasyTimer(int numTimers) {
+    maxTimers = numTimers;
+    this.maxTimers = maxTimers;
+}
+
+int EasyTimer::setInterval(unsigned long milliseconds, callback_function callback) {
+    return 0;
+}
+
+int EasyTimer::setTimeout(unsigned long milliseconds, callback_function callback) {
+    return 0;
+}
+
+void EasyTimer::update() {
+    
+}
---
+++ EasyTimer.h
@@ -0,0 +1,20 @@
+#ifndef EasyTimer_h
+#define EasyTimer_h
+
+class EasyTimer {
+
+    typedef void (*callback_function)();
+    
+    public:
+        EasyTimer(int numTimers);
+        int setInterval(unsigned long milliseconds, callback_function callback);
+        int setTimeout(unsigned long milliseconds, callback_function callback);
+        void update();
+
+    private:
+      int maxTimers;
+      
+        
+};
+
+#endif
